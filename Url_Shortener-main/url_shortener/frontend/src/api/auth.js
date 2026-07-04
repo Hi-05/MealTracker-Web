c5a6379c2df755149b429e34e41f075cef0df5f6
Jithendra Kumar Damera <jithendra.4446@gmail.com>
2026-07-04 21:32:49 +0530

    Delete Url_Shortener-main/url_shortener directory

--- Url_Shortener-main/url_shortener/frontend/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import client from "./client";
-
-export const register = (username, password) =>
-  client.post("/register", { username, password });
-
-export const login = async (username, password) => {
-  const res = await client.post("/login", { username, password });
-  return res.data;
-};
