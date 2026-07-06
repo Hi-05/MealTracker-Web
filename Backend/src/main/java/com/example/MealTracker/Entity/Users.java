79aa5552b37368cd3c0373b29d8b24f39ed2556f
Hi-05 <jithendra.4446@gmail.com>
2026-07-06 21:20:39 +0530

    solved authentication issues

--- Backend/src/main/java/com/example/MealTracker/Entity/Users.java
+++ Backend/src/main/java/com/example/MealTracker/Entity/Users.java
@@ -20,6 +20,7 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String username;
     private String password ;
 
--- Backend/src/main/java/com/example/MealTracker/Service/UserRegisterService.java
+++ Backend/src/main/java/com/example/MealTracker/Service/UserRegisterService.java
@@ -4,11 +4,13 @@ import com.example.MealTracker.Entity.Users;
 import com.example.MealTracker.Repository.UsersRepo;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UserRegisterService {
@@ -24,6 +26,13 @@ public class UserRegisterService {
     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder() ;
 
     public Users register(Users user){
+
+        if(repo.findByUsername(user.getUsername()) != null){
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "Username already exists"
+            );
+        }
 
         user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
